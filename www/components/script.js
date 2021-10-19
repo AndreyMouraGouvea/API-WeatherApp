0e5b10aaf148afc4d80ac2947be69d78dcd6f6ec
AndreyMouraGouvea <andreygouveia.05@gmail.com>
2021-10-20 00:23:17 +0900

    Calling api

--- www/components/script.js
+++ www/components/script.js
@@ -1,4 +1,26 @@
-// This is a JavaScript file
+// Calling api
+const api = {
+  key: "70b852138f2c472c9b34d8187d9a9fba",
+  base: "https://api.openweathermap.org/data/2.5/",
+  lang: "pt-br",
+  units: "metric"
+}
+// Calling id's as consts
+const city = document.querySelector('.city');
+const date = document.querySelector('.date');
+const container_img = document.querySelector('.container-img');
+const container_temp = document.querySelector('.container-temp');
+const temp_number = document.querySelector('.container-temp div');
+const temp_unit = document.querySelector('.container-temp span');
+const weather_t = document.querySelector('.weather');
+const search_input = document.querySelector('.form-control');
+const seach_button = document.querySelector('.btn');
+const low_high = document.querySelector('.low-high');
+
+
+
+
+
 const div = document.createElement('div');
 div.innerHTML = "<big>Andrey é demais!</big>"
 function teste(){
